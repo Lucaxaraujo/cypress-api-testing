ca59a1c61a70cd6e1c876a204b72fa225f1f0d10
Lucaxaraujo <lucasm030@gmail.com>
2023-06-26 23:33:49 -0300

    Fixed post session custom command location

--- projects/mark85-cypress-api/cypress/support/commands.js
+++ projects/mark85-cypress-api/cypress/support/commands.js
@@ -31,4 +31,13 @@ Cypress.Commands.add('postUser', (user) => {
     body: user,
     failOnStatusCode: false
   }).then(response => { return response })
-})
+});
+
+Cypress.Commands.add('postSession', (user) => {
+  cy.api({
+    url: '/sessions',
+    method: 'POST',
+    body: { email: user.email, password: user.password },
+    failOnStatusCode: false
+  }).then(response => { return response })
+});
